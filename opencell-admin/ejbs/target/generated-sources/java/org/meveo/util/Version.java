55df297ed3b61a08a79d6567fcfaeb25ce89c478
paulemmanueldelattre <paul-emmanuel.delattre@cgi.com>
2017-08-14 14:24:30 +0200

    Sonar analysis enabled
    Enabled via SonarQube Runner.
    Configuration in sonar-project.properties

--- opencell-admin/ejbs/target/generated-sources/java/org/meveo/util/Version.java
+++ opencell-admin/ejbs/target/generated-sources/java/org/meveo/util/Version.java
@@ -2,8 +2,8 @@ package org.meveo.util;
 
 public final class Version {
 
-    public static final String build_time="2017-06-10T17:39:48Z";
+    public static final String build_time="2017-08-10T14:55:34Z";
 
-    public static final String appVersion="4.5";
+    public static final String appVersion="4.5.1";
 
 }
